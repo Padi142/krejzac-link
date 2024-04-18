d5f5d56e623445b3172e703a9d4867c85eba2887
Padias <matyas.krejza@seznam.cz>
2024-04-18 21:38:45 +0200

    DB MIGRATION

--- src/lib/db/connection.server.ts
+++ src/lib/db/connection.server.ts
@@ -1,11 +1,11 @@
-import "dotenv/config";
-import { drizzle } from 'drizzle-orm/mysql2';
-import { migrate } from 'drizzle-orm/mysql2/migrator';
-import mysql from 'mysql2/promise';
+import { drizzle } from 'drizzle-orm/postgres-js';
 
-// Create the connection
-const poolConnection = mysql.createPool(process.env.MYSQL_URL as string);
+import { DATABASE_URL } from '$env/static/private';
+import postgres from 'postgres';
 
-export const drizzle_db = drizzle(poolConnection);
+
+
+export const client = postgres(DATABASE_URL);
+export const drizzle_db = drizzle(client);
 
 	// await migrate(drizzle_db, { migrationsFolder: 'drizzle' });
